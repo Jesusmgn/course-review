4843499a7991449bffc61a0ed68d6716d55f34a9
Jesusmgn <Jesusmgn1001@gmail.com>
2025-11-11 18:46:31 -0600

    ⭐FEAT: Agregados modelos Course y Review con sus migraciones

---
+++ app/Models/Course.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'instructor',
+    ];
+
+    //Un curso tiene muchas reseñas
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
